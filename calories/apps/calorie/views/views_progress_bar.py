5d6e9ff92cda23317b010034cadbd45e3b0dad21
Gabriel Lustosa Queiroz <lustosaki2@gmail.com>
2022-08-02 16:53:21 -0300

    FIX[VIEW]: Adicionado view

---
+++ calories/apps/calorie/views/views_progress_bar.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+
+from calories.apps.calorie.models import NutritionalGoal, NutritionalDayGoal
+from utils.nutritional import (
+    get_current_calories,
+    get_max_calories,
+    get_current_water,
+    get_max_water,
+    get_nutritional_day_goal_query
+)
+
+
+def render_progress_calories(request):
+    goal = NutritionalGoal.objects.filter(creator=request.user, active=True).first()
+
+    current_calories = get_current_calories(request.user, goal)
+    max_calories = get_max_calories(goal)
+
+    return render(request, 'calorie/includes/goal/progressbars/calories.html', context={
+        'current_calories': current_calories,
+        'max_calories': max_calories,
+    })
+
+
+def render_progress_water(request):
+    goal = NutritionalGoal.objects.filter(creator=request.user, active=True).first()
+
+    current_water = get_current_water(request.user)
+    total_water = get_max_water(goal)
+
+    return render(request, 'calorie/includes/goal/progressbars/water.html', context={
+        'current_water': current_water,
+        'total_water': total_water,
+    })
+
+
+def render_progress_nutritional(request):
+    goal = NutritionalGoal.objects.filter(creator=request.user, active=True).last()
+
+    options = ('protein', 'carbohydrate', 'fat')
+
+    goal_info = dict()
+    if goal:
+        day_goal_query = get_nutritional_day_goal_query(request.user)
+
+        if day_goal_query.exists():
+            day_goal = day_goal_query.first()
+        else:
+            day_goal = NutritionalDayGoal.objects.create(goal=goal)
+
+        for option in options:
+            goal_info[f'#{option}'] = [round(getattr(day_goal, option)), round(getattr(goal, option))]
+
+    if not goal_info:
+        for option in options:
+            goal_info[f'#{option}'] = [0, 0]
+
+    return render(request, 'calorie/includes/goal/progressbars/nutritional.html', context={'goal_info': goal_info})
+
+
+def render_progress_body(request):
+    return render(request, 'calorie/includes/goal/progressbars/body.html')
